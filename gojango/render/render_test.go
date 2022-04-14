670a36fb4fb33e73560dda0103114d23b270230f
Alejandro Saucedo <axsauze@gmail.com>
2022-04-14 16:09:03 +0200

    MOved tests to single function

--- gojango/render/render_test.go
+++ gojango/render/render_test.go
@@ -6,25 +6,42 @@ import (
 	"testing"
 )
 
+var pageData = []struct{
+	name string
+	renderer string
+	template string
+	errorExpected bool
+	errorMessage string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page_no_template", "go", "no-file", true, "no error rendering non-existing jet template"},
+	{"jet_page", "jet", "home", false, "error rendering go template"},
+	{"jet_page_no_template", "jet", "no-file", true, "no error rendering non-existing jet template"},
+	{"invalid_render_engine", "foo", "no-file", true, "no error rendering invalid template engine"},
+}
+
 func TestRender_Page(t *testing.T) {
-	r, err := http.NewRequest("GET", "/test-url", nil)
-	if err != nil {
-		t.Error(err)
-	}
+	for _, e := range pageData{
+		r, err := http.NewRequest("GET", "/test-url", nil)
+		if err != nil {
+			t.Error(err)
+		}
 
-	w := httptest.NewRecorder()
+		w := httptest.NewRecorder()
 
-	testRenderer.Renderer = "go"
-	testRenderer.RootPath = "./testdata"
+		testRenderer.Renderer = e.renderer
+		testRenderer.RootPath = "./testdata"
 
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering page", err)
-	}
-
-	testRenderer.Renderer = "jet"
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering page", err)
+		err = testRenderer.Page(w, r, e.template, nil, nil)
+		if e.errorExpected {
+			if err == nil {
+				t.Errorf("%s: %s", e.name, e.errorMessage)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: %s: %s", e.name, e.errorMessage, err.Error())
+			}
+		}
 	}
 }
+
