a8ed74414a91f374a5fff37f1977356be741c59a
Alejandro Saucedo <axsauze@gmail.com>
2022-05-28 17:42:37 +0100

    added base functionality for form get

--- gojango-myapp/routes.go
+++ gojango-myapp/routes.go
@@ -22,6 +22,9 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
 	a.App.Routes.Get("/users/logout", a.Handlers.Logout)
 
+	a.App.Routes.Get("/form", a.Handlers.Form)
+	//a.App.Routes.Post("/form", a.Handlers.PostForm)
+
 	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
 		u := data.User{
 			FirstName: "A",
@@ -73,7 +76,21 @@ func (a *application) routes() *chi.Mux {
 		}
 
 		u.LastName = a.App.RandomString(10)
+
+		validator := a.App.Validator(nil)
+		u.LastName = ""
+		u.Validate(validator)
+
+		if !validator.Valid() {
+			fmt.Fprint(w, "failed validation")
+			return
+		}
+
 		err = u.Update(*u)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
 
 		fmt.Fprintf(w, "updated last name to %s", u.LastName)
 	})
