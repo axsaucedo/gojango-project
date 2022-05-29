3dda0b10cbd5bf3a2759960b51cf3ad0a0c50af1
Alejandro Saucedo <axsauze@gmail.com>
2022-05-29 14:32:50 +0100

    Added utility helpers

--- gojango-myapp/routes.go
+++ gojango-myapp/routes.go
@@ -13,19 +13,19 @@ func (a *application) routes() *chi.Mux {
 	// middleware must come before any routes
 
 	// adding routes here
-	a.App.Routes.Get("/", a.Handlers.Home)
-	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
-	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
-	a.App.Routes.Get("/sessions", a.Handlers.SessionTestPage)
+	a.get("/", a.Handlers.Home)
+	a.get("/go-page", a.Handlers.GoPage)
+	a.get("/jet-page", a.Handlers.JetPage)
+	a.get("/sessions", a.Handlers.SessionTestPage)
 
-	a.App.Routes.Get("/users/login", a.Handlers.UserLogin)
-	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
-	a.App.Routes.Get("/users/logout", a.Handlers.Logout)
+	a.get("/users/login", a.Handlers.UserLogin)
+	a.post("/users/login", a.Handlers.PostUserLogin)
+	a.get("/users/logout", a.Handlers.Logout)
 
-	a.App.Routes.Get("/form", a.Handlers.Form)
-	a.App.Routes.Post("/form", a.Handlers.PostForm)
+	a.get("/form", a.Handlers.Form)
+	a.post("/form", a.Handlers.PostForm)
 
-	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
+	a.get("/create-user", func(w http.ResponseWriter, r *http.Request) {
 		u := data.User{
 			FirstName: "A",
 			LastName:  "S",
@@ -43,7 +43,7 @@ func (a *application) routes() *chi.Mux {
 		fmt.Fprintf(w, "%d: %s", id, u.FirstName)
 	})
 
-	a.App.Routes.Get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
+	a.get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
 		users, err := a.Models.Users.GetAll()
 		if err != nil {
 			a.App.ErrorLog.Println(err)
