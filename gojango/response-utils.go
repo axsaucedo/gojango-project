9fdae932bab7d0018f2b7ad8da93cae068936f41
Alejandro Saucedo <axsauze@gmail.com>
2022-05-29 14:44:01 +0100

    Added utility helpers

--- gojango/response-utils.go
+++ gojango/response-utils.go
@@ -2,7 +2,10 @@ package gojango
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"fmt"
 	"net/http"
+	"path"
 )
 
 func (g *Gojango) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
@@ -25,4 +28,54 @@ func (g *Gojango) WriteJSON(w http.ResponseWriter, status int, data interface{},
 	}
 
 	return nil
+}
+
+func (g *Gojango) WriteXML(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := xml.MarshalIndent(data, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gojango) DownloadFile(w *http.ResponseWriter, r *http.Request, pathToFile, fileName string) error {
+	fp := path.Join(pathToFile, fileName)
+	fileToServe := filePath.Clean(fp)
+	w.Header().Set("Content-Type", fmt.Sprintf("attachment; file=\"%s\"", fileName))
+	http.ServeFile(w, r, fileToServe)
+	return nil
+}
+
+func (g *Gojango) Error404(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusNotFound)
+}
+
+func (g *Gojango) Error500(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusInternalServerError)
+}
+
+func (g *Gojango) Error401(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusUnauthorized)
+}
+
+func (g *Gojango) Error403(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusForbidden)
+}
+
+func (g *Gojango) ErrorStatus(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
 }
